a85ad5ad825c3b520fd9a4528fcdf361e935dd46
SAKSHI KULSHRESHTHA <36589747+sakshi-kst@users.noreply.github.com>
2026-03-23 21:54:04 +0530

    Add files via upload

--- src/scenario5.py
+++ src/scenario5.py
@@ -1,20 +1,48 @@
 # Scenario 5: Airflow DAG Design
 
 from airflow import DAG
+from airflow.utils.context import Context
 from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 from datetime import datetime, timedelta
 
 
+# Method to notify failures via Slack message
+def notify_failure(context: Context):
+    task_id = context.get('task_instance').task_id
+    dag_id = context.get('task_instance').dag_id
+    log_url = context.get('task_instance').log_url
+    
+    slack_msg = f"""
+        :red_circle: Task: {task_id} in {dag_id} DAG failed.
+        View error log: {log_url}
+    """
+    
+    failed_alert = SlackWebhookOperator(
+        task_id = "notify_failure_on_slack",
+        slack_webhook_conn_id = "slack_default",
+        message = slack_msg,
+        channel = "#airflow-notifications",
+        username = "Airflow User"
+    )
+
+    return failed_alert.execute(context=context)
+
+
 # Dummy functions to be called by DAG Task
 def detect_new_files():
+    # This python function looks for a new file / input data, that needs to be processed.
+    # We can also make use of FileSensor instead of writing a PythonOperator.
     pass
 
 def validate():
+    # This python function runs validation checks on the new data, before ingestion.
+    # It can be used to trigger the Hard / Soft validation checks or the Great Expectations Suite.
     pass
 
 def reconcile():
+    # This python function generates the reconciliation metrics, after the data ingestion has completed.
     pass
 
 
@@ -35,12 +63,14 @@ with DAG(
 
     detect = PythonOperator(
         task_id = "detect_file",
-        python_callable = detect_new_files
+        python_callable = detect_new_files,
+        on_failure_callback = notify_failure
     )
 
     validate = PythonOperator(
         task_id = "validate",
-        python_callable = validate
+        python_callable = validate,
+        on_failure_callback = notify_failure
     )
 
     ingest = SparkSubmitOperator(
@@ -48,20 +78,22 @@ with DAG(
         application = "/path/to/spark_job.py",
         executor_cores = 4,
         executor_memory = "5G",
-        num_executors = 20
+        num_executors = 20,
+        on_failure_callback = notify_failure
     )
 
     reconcile = PythonOperator(
         task_id = "reconcile",
-        python_callable = reconcile
+        python_callable = reconcile,
+        on_failure_callback = notify_failure
     )
 
-    notify = SlackWebhookOperator(
-        task_id = "notify_on_slack",
+    notify_success = SlackWebhookOperator(
+        task_id = "notify_success_on_slack",
         slack_webhook_conn_id = "slack_default",
         message = "Data Ingestion workflow completed successfully!",
         channel = "#airflow-notifications",
         username = "Airflow User"
     )
 
-detect >> validate >> ingest >> reconcile >> notify
+detect >> validate >> ingest >> reconcile >> notify_success
